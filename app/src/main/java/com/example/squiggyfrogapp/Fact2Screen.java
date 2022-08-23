91a6a6d848162dc34ee4f3016653b496ced924a4
Maddison <mtranchau1@gmail.com>
2022-08-22 19:39:08 -0700

    8/22/22: 7:38pm - Created unreachable third screen (fact #2 screen) that has the same layout as the previous fact screen.  Next steps: make next button on first fact screen functional so that it sends the user to the second fact screen when pressed

---
+++ app/src/main/java/com/example/squiggyfrogapp/Fact2Screen.java
@@ -0,0 +1,14 @@
+package com.example.squiggyfrogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fact2Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fact2_screen);
+    }
+}
