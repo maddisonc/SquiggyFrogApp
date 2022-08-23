6973b760dba7a6c7a7184626b2a7ab6e78c6d3b8
Maddison <mtranchau1@gmail.com>
2022-08-22 19:31:32 -0700

    8/22/22: 7:30pm - The start button is now functional (pressing it sends the user to the first fact screen.)  Also created the first fact screen that contains a title textView, an image, one fact textView, and a next button (that is not yet functional).

--- app/src/main/java/com/example/squiggyfrogapp/MainActivity.java
+++ app/src/main/java/com/example/squiggyfrogapp/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.squiggyfrogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button)findViewById(R.id.startBtn);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startBtnPressed();
+            }
+        }); // listener that calls next activity method
+
     }
+
+    private void startBtnPressed ()
+    {
+        Button startButton = (Button) findViewById(R.id.startBtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                startActivity(new Intent(MainActivity.this, Fact1Screen.class));
+
+            } // end onCLick
+        });
+    } // end start button handler
+
+
 }
